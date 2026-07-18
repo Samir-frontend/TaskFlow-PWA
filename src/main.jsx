72ddcda09e9fe94ab884618026a66e52037fad4d
Samir Sheikh <samirshiekh0808@gmail.com>
2026-07-18 09:42:37 +0530

    Fix GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { HashRouter } from "react-router-dom";
+
 import { Toaster } from "react-hot-toast";
 import { registerSW } from "virtual:pwa-register";
 
-registerSW({
-  immediate: true,
-});
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+
+import { TaskProvider } from "./context/TaskContext";
+import { ThemeProvider } from "./context/ThemeContext";
+
 import "./registerSW";
 
 import "./styles/globals.css";
@@ -21,25 +23,31 @@ import "./styles/form.css";
 import "./styles/footer.css";
 import "./styles/responsive.css";
 
-import { TaskProvider } from "./context/TaskContext";
-import { ThemeProvider } from "./context/ThemeContext";
+registerSW({
+  immediate: true,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <TaskProvider>
-        <BrowserRouter>
-  <App />
-  <Toaster
-    position="top-right"
-    toastOptions={{
-      duration: 3000,
-      style: {
-        borderRadius: "12px",
-      },
-    }}
-  />
-</BrowserRouter>
+
+        <HashRouter>
+
+          <App />
+
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 3000,
+              style: {
+                borderRadius: "12px",
+              },
+            }}
+          />
+
+        </HashRouter>
+
       </TaskProvider>
     </ThemeProvider>
   </React.StrictMode>
