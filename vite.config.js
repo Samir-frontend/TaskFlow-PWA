e48318ee08bf608ee81909ed909b0567ec281511
Samir Sheikh <samirshiekh0808@gmail.com>
2026-07-18 09:24:01 +0530

    Fix GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  // GitHub Repository Name
+  base: "/TaskFlow-PWA/",
+
   plugins: [
     react(),
+
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "auto",
@@ -13,22 +17,28 @@ export default defineConfig({
         name: "TaskFlow PWA",
         short_name: "TaskFlow",
         description: "Task Management Progressive Web App",
+
         theme_color: "#16a34a",
         background_color: "#166534",
+
         display: "standalone",
+        orientation: "portrait",
+
+        start_url: "/TaskFlow-PWA/",
+        scope: "/TaskFlow-PWA/",
 
         icons: [
-        {
-         src: "/icons/icon-192.png",
-         sizes: "192x192",
-         type: "image/png"
-        },
-       {
-        src: "/icons/icon-512.png",
-        sizes: "512x512",
-        type: "image/png"
-       }
-      ]
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
       },
 
       workbox: {
